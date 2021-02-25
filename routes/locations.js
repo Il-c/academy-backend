e224598b1c592bd0afd0741f7acf1ff5e14af440
Ilari Lehtinen <ilari.lehtinen@eficode.com>
2021-02-25 14:43:50 +0200

    Modify frontend

--- routes/locations.js
+++ routes/locations.js
@@ -4,17 +4,21 @@ let database = [
   { id: 1, latitude: 60, longitude: 70 },
   { id: 2, latitude: 40, longitude: 80 },
 ];
-// middleware that is specific to this router
+
 router.use(function timeLog(req, res, next) {
   console.log("Time: ", Date.now());
   next();
 });
-// define the home page route
+
 router.get("/", function (req, res) {
+  res.json(database);
+});
+
+router.get("/pretty", function (req, res) {
   res.header("Content-Type", "application/json");
   res.send(JSON.stringify(database, null, 2));
 });
-// define the about route
+
 router.get("/1", function (req, res) {
   res.send(database[0]);
 });
